2539bce4aae028acfb60e3a685368d196e282a4e
Ruii1112 <ruiiiii1112@gmail.com>
2026-04-23 00:10:16 +0900

    修正

--- backend/src/test/java/com/example/knowledge/presentation/controller/AuthControllerTest.java
+++ backend/src/test/java/com/example/knowledge/presentation/controller/AuthControllerTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,7 +24,7 @@ class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private AuthSignupUsecase usecase;
 
     @Test
